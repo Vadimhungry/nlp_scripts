72504c86713aadfd012257f15821839e9fda8509
VadimK <yaralavadim@gmail.com>
2024-06-03 17:34:22 +0300

    initial

---
+++ src/alice.py
@@ -0,0 +1,51 @@
+import string
+import re
+import nltk
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+from nltk.probability import FreqDist
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
+
+f = open('books/carrol/alice_in_wonderland.txt', "r", encoding="utf-8")
+text = f.read()
+
+text = text.lower()
+spec_chars = string.punctuation
+text = "".join([ch for ch in text if ch not in spec_chars])
+text = re.sub('\n', ' ', text)
+
+
+
+text_tokens = word_tokenize(text)
+
+text = nltk.Text(text_tokens)
+
+english_stopwords = stopwords.words("english")
+english_stopwords.extend(['one', 'im', 'said'])
+
+text_tokens = [token.strip() for token in text_tokens if token not in english_stopwords]
+
+text = nltk.Text(text_tokens)
+
+fdist_sw = FreqDist(text)
+
+# print the most common words
+# print(fdist_sw.most_common(5))
+
+text_raw = " ".join(text)
+wordcloud = WordCloud(width=1600, height=800, background_color='white', colormap='gist_rainbow_r').generate(text_raw)
+plt.figure(figsize=(20, 10), facecolor='r')
+
+# добавляем туда облако слов
+plt.imshow(wordcloud)
+
+# выключаем оси и подписи
+plt.axis("off")
+
+# убираем рамку вокруг
+plt.tight_layout(pad=0)
+
+# выводим картинку на экран
+plt.show()
